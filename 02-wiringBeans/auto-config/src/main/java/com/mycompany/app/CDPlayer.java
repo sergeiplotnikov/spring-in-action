3683fdf532d297ad97a95b3e6226c1514e41d154
sergeiplotnikov <sergei.plotnikov128@gmail.com>
2017-05-22 05:55:52 +0300

    02 - Wiring Beans - autoconfig

---
+++ 02-wiringBeans/auto-config/src/main/java/com/mycompany/app/CDPlayer.java
@@ -0,0 +1,33 @@
+package com.mycompany.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//import javax.inject.Inject;
+//import javax.inject.Named;
+//another JSR-330 example is SgtPeppers.java file
+
+@Component
+//@Named
+//A JSR-330 alternative annotation
+public class CDPlayer implements MediaPlayer{
+	private CompactDisc cd;
+	private MetalMusicCD metalCD;
+	
+	@Autowired
+	//@Inject
+	//A JSR-330 alternative annotation
+	public CDPlayer(CompactDisc cd){
+		this.cd = cd;
+	}
+	
+	public void play(){
+		cd.play();
+	}
+	
+	@Autowired(required=false)
+	//unless 'required' set to 'false' the MAVEN PACKAGE goal will fail test. Since there is no bean that implements MetalMusicCD
+	public void putInMetalMusicCD(MetalMusicCD metal){
+		this.metalCD = metal;
+	}
+}
---
+++ 02-wiringBeans/auto-config/src/main/java/com/mycompany/app/SgtPeppers.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import org.springframework.stereotype.Component;
+//import javax.inject.Named;
+
+@Component("lonelyHeartsClub")
+//the string is the components id
+//@Named("lonelyHeartsClub")
+//@Named can be used as alternative to @Component
+//part of JSR-330, in order to make functional: a) uncomment 'Named' import. b) uncomment 'apache.geronimo' dependency in pom.xml
+//Spring in Action, 4th ed : Spring supports the @Named annotation as an alternative to @Component. There are a few subtle differences, but in most cases
+//	they are interchangable.
+public class SgtPeppers implements CompactDisc{
+	private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+	private String artist = "The Beatles";
+	
+	public void play(){
+		System.out.print("Playing " + title + " by " + artist);
+	}
+}
